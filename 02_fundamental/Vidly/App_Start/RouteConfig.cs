e58d9e12a85f5ff74a14c4813ac06da7ff91267f
unknown <bryanlibl20@gmail.com>
2019-07-11 16:27:30 -0400

    ASP,NET basic

---
+++ 02_fundamental/Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes(); // remove the following one
+
+            ////Route order matter, put routes before the default one
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate"},
+            //    //adding fields restraints
+            //    //new { year = @"\d{4}", month = @"\d{2}"});
+            //    //only2015 or 2016
+            //    new { year = @"2015|2016", month = @"\d{2}" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
